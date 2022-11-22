d69dfdd9bb5351482fa2f36da63e1037e768f0f4
stevesun <sunguoqin@oneinfitech.com>
2022-11-21 18:29:26 -0800

    add sign up page and ui update

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import md5 from 'md5'
 import "./LoginPage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 const LoginPage = () => {
+  const [pageStatus, setPageStatus] = useState("login")
+
+  useEffect(() => {
+    
+  }, [pageStatus])
+
   const login = async (e) => {
     e.preventDefault();
-    let requestAPI = "/login/password"
+    let requestAPI = "/user/login"
+    let hashedPwd = md5(e.target.password.value)
+    console.log(hashedPwd)
+    let res = await fetch(requestAPI, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          username: e.target.username.value,
+          password: e.target.password.value,
+        }
+      )
 
-    // let res = fetch(requestAPI, {
-    //   method: "POST",
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(
-    //     {
-    //       username: e.target.username.value,
-    //       password: e.target.password.value,
-    //     }
-    //   )
-
-    // })
-    // let data = await res.json()
-    // console.log(data)
+    })
+    let data = await res.json()
+    console.log(data)
     // // TODO
     // console.log("res in login page", res.data);
     window.localStorage.setItem("email", e.target.username.value);
   };
   return (
-    <div className="back-page">
+    pageStatus ? (// true => sign in
+      <div className="back-page">
       <div className="wrapper">
 
         <div className="text-center mt-4 name">Pocket Recipe</div>
@@ -64,13 +74,61 @@ const LoginPage = () => {
             Sign in
           </button>
         </form>
-        <div className="text-center fs-6">
+        <div className="text-center fs-6 ">
 
-          <a href="">Sign up</a>
+          <a className='small-text' onClick={() => {setPageStatus(false)}}>Sign Up</a>
         </div>
       </div>
     </div >
+    ) : (//false => sign up
+      <div className="back-page">
+      <div className="wrapper">
+      <div className="text">
+        <a className='small-text' onClick={() => {setPageStatus(true)}}>&lt; back</a>
+      </div>
+
+        <div className="text-center mt-4 name">Create new account</div>
+        <form onSubmit={login} className="p-3 mt-3">
+          <div>
+            <label className="form-label" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="form-control"
+              id="username"
+              name="username"
+              type="text"
+              autoComplete="username"
+              required
+              autoFocus
+            />
+          </div>
+          <div>
+            <label className="form-label" htmlFor="current-password">
+              Password
+            </label>
+            <input
+              className="form-control"
+              id="current-password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+            />
+          </div>
+          <br />
+          <button className="btn btn-primary" type="submit">
+            Sign up
+          </button>
+        </form>
+        
+      </div>
+    </div >
+    )
+    
   );
 };
+
+
 
 export default LoginPage;
