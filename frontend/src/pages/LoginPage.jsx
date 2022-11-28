ceed296df9aab2f1c70c0c1b3a236d696b3fbff3
stevesun <sunguoqin@oneinfitech.com>
2022-11-27 19:09:00 -0800

    login debug and list UI change

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,104 +1,83 @@
-import React, { useState, useEffect } from 'react'
-import md5 from 'md5'
-import './LoginPage.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useState, useEffect } from "react";
+import md5 from "md5";
+import "./LoginPage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-import Alert from '@mui/material/Alert'
-import Stack from '@mui/material/Stack'
-
-
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
 
 const LoginPage = () => {
-  const [pageStatus, setPageStatus] = useState('login')
-  const [createState, setCreatestate] = useState('initialState')
-  useEffect(() => { }, [pageStatus])
-  useEffect(() => { }, [createState])
+  const [pageStatus, setPageStatus] = useState("login");
+  const [createState, setCreatestate] = useState("initialState");
+  useEffect(() => {}, [pageStatus]);
+  useEffect(() => {}, [createState]);
 
   const login = async (e) => {
-    e.preventDefault()
-    let requestAPI = '/user/login'
-    let hashedPwd = md5(e.target.password.value)
-    console.log(hashedPwd)
+    e.preventDefault();
+    let requestAPI = "/user/login";
+    let hashedPwd = md5(e.target.password.value);
+    console.log(hashedPwd);
     let res = await fetch(requestAPI, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(
-        {
-          email: e.target.username.value,
-          password: hashedPwd,
-        }
-      )
-
-    })
-    let data = await res.json()
+      body: JSON.stringify({
+        email: e.target.username.value,
+        password: hashedPwd,
+      }),
+    });
+    let data = await res.json();
     if (data.val === 1) {
-      console.log('here')
-      window.localStorage.setItem('email', e.target.username.value)
-      window.location.reload(true)
+      window.localStorage.setItem("email", e.target.username.value);
+      window.location.reload(true);
     } else {
-      console.log('not here')
-      document.getElementById('login-fail-alert').style.display = 'block'
+      document.getElementById("login-fail-alert").style.display = "block";
     }
-
-    // // TODO: judge if successful user login, if so, add to localstorage and refresh page
-
-  }
+  };
 
   const createUser = async (e) => {
-    e.preventDefault()
-    let requestAPI = '/user/create'
-    let hashedPwd = md5(e.target.password.value)
-    console.log('sign up email:', e.target.username.value)
-    console.log('sign up page:', hashedPwd)
+    e.preventDefault();
+    let requestAPI = "/user/create";
+    let hashedPwd = md5(e.target.password.value);
+    console.log("sign up email:", e.target.username.value);
+    console.log("sign up page:", hashedPwd);
 
     let res = await fetch(requestAPI, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(
-        {
-          email: e.target.username.value,
-          password: hashedPwd,
-        }
-      )
-
-    })
-    let data = await res.json()
-    console.log(data)
+      body: JSON.stringify({
+        email: e.target.username.value,
+        password: hashedPwd,
+      }),
+    });
+    let data = await res.json();
+    console.log(data);
     if (data.val == 1) {
-      console.log('here data is 1')
-      setCreatestate(false)
+      document.getElementById("create-success").style.display = "block";
+      setCreatestate(false);
 
-      setPageStatus(true)
+      // setPageStatus(true)
     } else {
-      document.getElementById('create-fail-alert').style.display = 'block'
-      setCreatestate(false)
+      document.getElementById("create-fail-alert").style.display = "block";
+      setCreatestate(false);
       // setPageStatus(true);
     }
-  }
-
+  };
 
   return pageStatus ? ( // true => sign in page
     <div className="back-page">
       <div className="wrapper">
-        <Stack sx={{ width: '100%' }} spacing={2}>
-          <Alert
-            id="create-success"
-            severity="success"
-            style={{ display: 'none' }}
-          >
-            Successfully created the account
-          </Alert>
+        <Stack sx={{ width: "100%" }} spacing={2}>
           <Alert
             id="login-fail-alert"
             severity="error"
-            style={{ display: 'none' }}
+            style={{ display: "none" }}
           >
-            Wrong email or password
+            Wrong user or password
           </Alert>
         </Stack>
 
@@ -140,7 +119,7 @@ const LoginPage = () => {
           <a
             className="small-text"
             onClick={() => {
-              setPageStatus(false)
+              setPageStatus(false);
             }}
           >
             Sign Up
@@ -156,16 +135,24 @@ const LoginPage = () => {
           <a
             className="small-text"
             onClick={() => {
-              setPageStatus(true)
+              setPageStatus(true);
             }}
           >
             &lt; back
           </a>
         </div>
         <Alert
+          id="create-success"
+          severity="success"
+          style={{ display: "none" }}
+        >
+          <AlertTitle>Successfully created the account</AlertTitle>
+          <strong>Now back to sign in page</strong>
+        </Alert>
+        <Alert
           id="create-fail-alert"
           severity="error"
-          style={{ display: 'none' }}
+          style={{ display: "none" }}
         >
           User already exists
         </Alert>
@@ -206,8 +193,8 @@ const LoginPage = () => {
         </form>
       </div>
     </div>
-  )
-}
-LoginPage.propTypes = {}
+  );
+};
+LoginPage.propTypes = {};
 
-export default LoginPage
+export default LoginPage;
