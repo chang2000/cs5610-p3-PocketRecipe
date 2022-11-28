6fb3a88c5e1673598b9f4e442e992b996d22120a
stevesun <sunguoqin@oneinfitech.com>
2022-11-27 17:22:39 -0800

    recipe list improvement

--- frontend/src/pages/RecipeList.jsx
+++ frontend/src/pages/RecipeList.jsx
@@ -1,32 +1,32 @@
-import '../App.css'
-import React, { useState, useEffect } from 'react'
-import { Link, Outlet } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import "../App.css";
+import React, { useState, useEffect } from "react";
+import { Link, Outlet } from "react-router-dom";
+import PropTypes from "prop-types";
 
 function RecipeList(props) {
-  const type = props.type
-  // the recipes stores ids of recipe of the current list 
-  const [recipes, setRecipes] = useState([])
-  const user = window.localStorage.getItem('email')
+  const type = props.type;
+  // the recipes stores ids of recipe of the current list
+  const [recipes, setRecipes] = useState([]);
+  const user = window.localStorage.getItem("email");
   // Do this once page reloaded
   useEffect(() => {
     // TODO: remove hardcode later
     const fetchData = async () => {
-      let requestAPI = ''
-      if (type === 'mine') {
-        requestAPI = `/item/getByUser?email=${user}`
-      } else if (type === 'discover') {
-        requestAPI = '/item/getAllPub'
-      } else if (type === 'fav') {
-        requestAPI = `/item/getFav?email=${user}`
+      let requestAPI = "";
+      if (type === "mine") {
+        requestAPI = `/item/getByUser?email=${user}`;
+      } else if (type === "discover") {
+        requestAPI = "/item/getAllPub";
+      } else if (type === "fav") {
+        requestAPI = `/item/getFav?email=${user}`;
       }
-      let res = await fetch(requestAPI)
-      let data = await res.json()
-      console.log(data)
-      setRecipes(data.recipes)
-    }
-    fetchData()
-  }, [type, user])
+      let res = await fetch(requestAPI);
+      let data = await res.json();
+      console.log(data);
+      setRecipes(data.recipes);
+    };
+    fetchData();
+  }, [type, user]);
 
   return (
     <>
@@ -34,25 +34,29 @@ function RecipeList(props) {
       <div id="recipe-list">
         {`Current List Type: ${type}`}
         <br></br>
-        {/* The following items should be dynamic */}
-        <div id="main-list">
-          {/* Dynamic render */}
-          {recipes.map((item, i) => (
-            <Link key={i} to={`/${type}/${item._id}`}>
-              {item.name}
-            </Link>
-          ))}
-        </div>
+        <nav>
+          <ul>
+            {/* The following items should be dynamic */}
+            {/* Dynamic render */}
+            {recipes.map((item, i) => (
+              <li>
+                <Link key={i} to={`/${type}/${item._id}`}>
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
       <div id="contact">
         <Outlet />
       </div>
     </>
-  )
+  );
 }
 
 RecipeList.propTypes = {
-  type: PropTypes.string.isRequired
-}
+  type: PropTypes.string.isRequired,
+};
 
-export default RecipeList
+export default RecipeList;
